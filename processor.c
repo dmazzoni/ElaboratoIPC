66cf4ee0bf7b7b3d5e89cf39f93a4382b313086b
Giacomo Annaloro <giacomoannaloro92@gmail.com>
2013-06-14 22:46:00 +0200

    Documentazione processor

--- processor.c
+++ processor.c
@@ -1,14 +1,38 @@
-#include <signal.h>
+/** @file
+	Code for the child processor, which is
+	started by the main process.<br>
+	Each processor is launched passing its ID and the
+	total number of semaphores: with this information the
+	processor can synchronize on the correct semaphore.<br>
+	After attaching the shared memory segments, it loops
+	while there are operations to compute, and writes the 
+	results in the operations array.
+*/
+
+#include <signal.h> 
 #include <stdlib.h>
 #include <sys/types.h>
 #include "io_utils.h"
 #include "ipc_utils.h"
 #include "operation.h"
 
+/**
+	IDs for the used IPCs
+	<ul><li>Cell 0: semaphore set
+	<li>Cell 1: shared memory for operations
+	<li>Cell 2: shared memory for processor states</ul>
+*/
 int ipc_id[] = {-1, -1, -1};
 
 static void compute(operation *oper);
 
+/**
+	Computes the operations while they are provided by 
+	the main processor and returns the results in the
+	corresponding shared memory locations, with synchronized access.
+	@param argc The number of arguments
+	@param argv The array of arguments
+*/
 int main(int argc, char *argv[]) {
 	int proc_id, nsems;
 	int *shm_states;
@@ -31,6 +55,11 @@ int main(int argc, char *argv[]) {
 	}
 }
 
+/**
+	Calculates the operation passed and stores the result in
+	the first operand field.
+	@param oper The operation to execute
+*/
 static void compute(operation *oper) {
 	switch(oper->op) {
 		case '+': oper->num1 = oper->num1 + oper->num2; break;
