d764488d94f7e66f0d8460c88765fd9a5587d73e
Davide Mazzoni <davidemazzoni@yahoo.it>
2013-06-15 21:26:30 +0200

    Documentazione ipc_utils

--- lib/ipc_utils.c
+++ lib/ipc_utils.c
@@ -1,3 +1,7 @@
+/** @file
+	Contains functions for IPC initialization and management.
+*/
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,6 +16,7 @@ extern int ipc_id[];
 static void sem_operation(int semnum, short op);
 static int generate_key(int seed);
 
+/// Used for calls to @c semctl()
 union semun {
 	int val;
 	struct semid_ds *buf;
@@ -19,6 +24,15 @@ union semun {
 	struct seminfo *__buf;
 };
 
+/**
+	Creates the semaphore set and the shared memory segments, 
+	saving their IDs into @c ipc_id
+	Wraps the @c semget() and @c shmget() system calls, checking their exit code.
+	@param nsems The number of semaphores
+	@param shm1_size The size of the shared memory for operations
+	@param shm2_size The size of the shared memory for processor states
+	@see ipc_id
+*/
 void init_ipc(int nsems, int shm1_size, int shm2_size, int flags) {
 
 	ipc_id[0] = semget(generate_key(1), nsems, flags);
@@ -40,6 +54,11 @@ void init_ipc(int nsems, int shm1_size, int shm2_size, int flags) {
 	}
 }
 
+/**
+	Closes the semaphore set and the shared memory segments.<br>
+	Wraps the @c semctl() and @c shmctl() system calls, checking their exit code.
+	@return 0 if all IPCs are successfully closed, -1 otherwise.
+*/
 int close_ipc(void) {
 	int i, ret = 0;
 	
@@ -51,6 +70,18 @@ int close_ipc(void) {
 	return ret;
 }
 
+/**
+	Initializes the semaphore set as follows:<ul>
+	<li>Semaphores used by the main process to wait for a specific 
+	processor are set to 1.
+	<li>Semaphores used by each processor to wait for an operation are set to 0.
+	<li>The semaphore which enforces mutually exclusive access to the
+	shared memory for processor states is set to 1.
+	<li>The semaphore used by the main process to wait when no
+	processors are available is set to @c processors.
+	</ul>Wraps the @c semctl() system call, checking its exit code.
+	@param processors The number of processors
+*/
 void init_sems(int processors) {
 	int i, nsems = 2 * processors + 2;
 	union semun args;
@@ -69,14 +100,29 @@ void init_sems(int processors) {
 	}
 }
 
+/**
+	Implements the @c wait operation on the specified semaphore.
+	@param semnum The semaphore to wait
+	@see sem_operation
+*/
 void sem_p(int semnum) {
 	sem_operation(semnum, -1);
 }
 
+/**
+	Implements the @c signal operation on the specified semaphore.
+	@param semnum The semaphore to signal
+	@see sem_operation
+*/
 void sem_v(int semnum) {
 	sem_operation(semnum, 1);
 }
 
+/**
+	Wraps the @c shmat() system call, checking its exit code.
+	@param shm_id The shared memory segment's ID
+	@return A pointer to the attached segment.
+*/
 void* shm_attach(int shm_id) {
 	void *ret = shmat(shm_id, NULL, 0);
 	if (ret == (void *) -1) {
@@ -86,11 +132,19 @@ void* shm_attach(int shm_id) {
 	return ret;
 }
 
+/**
+	Wraps the @c shmdt() system call, checking its exit code.
+	@param address The attached address to detach
+*/
 void shm_detach(void *address) {
 	if (shmdt(address) == -1)
 		perror("Failed to detach shared memory segment");
 }
 
+/**
+	Wraps the @c ftok() system call, checking its exit code.
+	@param seed The seed to generate the key
+*/
 static int generate_key(int seed) {
 	int key = ftok("main.c", seed);
 	if (key == -1) {
@@ -100,6 +154,12 @@ static int generate_key(int seed) {
 	return key;
 }
 
+/**
+	Performs the specified operation on the given semaphore.<br>
+	Wraps the @c semop() system call, checking its error code.
+	@param semnum The semaphore number
+	@param op The operation to perform
+*/
 static void sem_operation(int semnum, short op) {
 	struct sembuf sops;
 	
