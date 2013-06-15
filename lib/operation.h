66e1c83b57f484941e1ccf3b6a027b8ebef3744f
Davide Mazzoni <davidemazzoni@yahoo.it>
2013-06-15 15:54:59 +0200

    Documentazione operation

--- lib/operation.h
+++ lib/operation.h
@@ -1,9 +1,19 @@
+/** @file
+	Defines the structure of an operation, which is composed
+	of two integers and an operator.
+*/
+
 #ifndef OPERATION_H
 #define OPERATION_H
 
 typedef struct operation {
+	/// The first operand, also used to store the result
 	int num1;
+	
+	/// The operator, also used to pass the termination command
 	char op;
+	
+	/// The second operand
 	int num2;
 } operation;
 
